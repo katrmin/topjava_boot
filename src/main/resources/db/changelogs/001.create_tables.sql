643f7bba19a53f937a4a82b159a536ba5350437b
katrmin <katrmin@yandex.ru>
2022-05-13 00:36:07 +0300

    make liquibase migration

---
+++ src/main/resources/db/changelogs/001.create_tables.sql
@@ -0,0 +1,45 @@
+--liquibase formatted sql
+
+--changeset Minaeva E.A.:001.create_tables.sql-1
+create table USERS
+(
+    id       bigint auto_increment PRIMARY KEY,
+    name     VARCHAR(255),
+    email    VARCHAR(255),
+    password VARCHAR(255)
+);
+
+--changeset Minaeva E.A.:001.create_tables.sql-2
+create table USER_ROLES
+(
+    id      bigint auto_increment PRIMARY KEY,
+    role    VARCHAR(255),
+    user_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES USERS (ID)
+);
+
+--changeset Minaeva E.A.:001.create_tables.sql-3
+create table RESTAURANTS
+(
+    id   bigint auto_increment PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+--changeset Minaeva E.A.:001.create_tables.sql-4
+create table DISHES
+(
+    id   bigint auto_increment PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+--changeset Minaeva E.A.:001.create_tables.sql-5
+create table MENU_RESTAURANTS_DISHES
+(
+    id            bigint auto_increment PRIMARY KEY,
+    restaurant_id BIGINT,
+    dish_price    DECIMAL(9,2),
+    dish_id       BIGINT,
+    date_time     datetime,
+    FOREIGN KEY (dish_id) REFERENCES DISHES (ID),
+    FOREIGN KEY (restaurant_id) REFERENCES RESTAURANTS (ID)
+);
---
+++ src/main/resources/db/changelogs/002.insert_tables.sql
@@ -0,0 +1,44 @@
+--liquibase formatted sql
+
+--changeset Minaeva E.A.:002.insert_tables.sql
+INSERT INTO USERS (NAME, EMAIL, PASSWORD)
+VALUES ('User', 'user@yandex.ru', '{noop}password'),
+       ('Admin', 'admin@gmail.com', '{noop}admin'),
+       ('Guest', 'guest@gmail.com', '{noop}guest');
+
+INSERT INTO USER_ROLES (ROLE, USER_ID)
+VALUES ('USER', 1),
+       ('ADMIN', 2),
+       ('USER', 2);
+
+insert into RESTAURANTS(name)
+values ('Первый ресторан'),
+        ('Второй ресторан'),
+        ('Третий ресторан');
+
+INSERT INTO DISHES (name)
+VALUES ('Яичница-глазунья'),
+       ('Борщ украинский'),
+       ('Ватрушки к борщу'),
+       ('Бефстроганов из говядины с пюре'),
+       ('Пирожное'),
+       ('Компот'),
+       ('Чай'),
+       ('Кофе');
+
+insert into MENU_RESTAURANTS_DISHES(restaurant_id,
+                                    dish_id,
+                                    dish_price,
+                                    date_time)
+values (1, 1, 500.30, '2022-05-12 10:00:00'),
+       (1, 2, 330.30, '2022-05-12 10:01:00'),
+       (1, 3, 60.0, '2022-05-12 10:02:00'),
+       (1, 6, 100.0, '2022-05-12 09:03:00'),
+       (2, 1, 300.30, '2022-05-12 09:00:00'),
+       (2, 2, 230.30, '2022-05-12 09:01:00'),
+       (2, 3, 40.0, '2022-05-12 09:02:00'),
+       (2, 6, 80.0, '2022-05-12 09:03:00'),
+       (3, 4, 400.30, '2022-05-12 09:30:00'),
+       (3, 2, 230.30, '2022-05-12 09:31:00'),
+       (3, 3, 40.0, '2022-05-12 09:32:00'),
+       (3, 7, 100.0, '2022-05-12 09:33:00')
