4794d0f81f8f3053411d30ec926ec5e6a32e5dce
katrmin <katrmin@yandex.ru>
2022-05-16 03:11:49 +0300

    deleted from json unset  fields

--- src/main/java/ru/javaops/topjava/dto/RestaurantDto.java
+++ src/main/java/ru/javaops/topjava/dto/RestaurantDto.java
@@ -1,5 +1,6 @@
 package ru.javaops.topjava.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -9,5 +10,6 @@ import lombok.experimental.SuperBuilder;
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 @NoArgsConstructor
+@JsonInclude(value = JsonInclude.Include.NON_EMPTY, content = JsonInclude.Include.NON_NULL)
 public class RestaurantDto extends NamedDto {
 }
